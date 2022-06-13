87b56c3a6bd4a5dd62cf30e92ca3191abb3cd32c
santocaridi99 <santocaridi99@gmail.com>
2022-06-13 10:30:58 +0200

    Aggiungere i file di progetto.

---
+++ csharp-bibliotecaMvc/Models/Autore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace csharp_bibliotecaMvc.Models
+{
+    public class Autore
+    {
+        [Column(Order = 1), System.ComponentModel.DataAnnotations.Key]
+        public string Cognome { get; set; }
+        [Column(Order = 2), System.ComponentModel.DataAnnotations.Key]
+        public string Nome { get; set; }
+        [Column(Order = 3), System.ComponentModel.DataAnnotations.Key]
+        public DateTime DataNascita { get; set; }
+
+        public ICollection<Libro>? Libri { get; set; }
+
+    }
+}
